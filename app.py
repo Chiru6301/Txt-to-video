87d47c27ca093371d14297e5aa921103119146ad
Chiru6301 <chiruedizz@gmail.com>
2025-02-26 22:12:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def hello_world():
 	<br></br><br></br><br></br>
 	<footer class="bg-dark text-white text-center py-3 mt-5">
 	<center><img loading="lazy" class="object-none object-center" src="" width="60" height="60">
-        Powered By NIKHIL 
+        Powered By CHIRU 
 		<img loading="lazy" class="object-none object-center" src="" width="60" height="60">
 		<div class="footer__copyright">
             <p class="footer__copyright-info">
